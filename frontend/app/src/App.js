d728d2924964aaadb887b04a84350679301b15f4
roxy wilcox <roxygwilcox@gmail.com>
2020-05-09 20:21:31 -0700

    working examples of line graph, bar graph, and polar chart

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -8,7 +8,7 @@ const graph1 = {
   labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
   datasets: [
     {
-      label: "Number of Unemployment Claims",
+      label: "Number of Cases",
       backgroundColor: "rgba(75,192,192,0.2)",
       borderColor: "rgba(255,99,132,1)",
       borderWidth: 3,
@@ -17,7 +17,7 @@ const graph1 = {
       data: [2, 1, 2, 1, 1, 1],
     },
     {
-      label: "Number of Cases",
+      label: "Number of Unemployment Claims",
       data: [10, 20, 12, 15, 10, 15],
       fill: false,
       borderColor: "#742774",
@@ -30,7 +30,7 @@ const graph2 = {
   datasets: [
     {
       label: "Number of Cases",
-      data: [10, 20, 12, 15, 10, 15],
+      data: [1, 3, 6, 10, 20, 36],
       backgroundColor: "rgba(255,99,132,0.2)",
       borderColor: "rgba(255,99,132,1)",
       borderWidth: 1,
@@ -67,3 +67,71 @@ export default function App() {
     </div>
   );
 }
+/*
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { MDBContainer } from "mdbreact";
+
+class ChartsPage extends React.Component {
+  state = {
+    dataLine: {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "My First dataset",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(225, 204,230, .3)",
+          borderColor: "rgb(205, 130, 158)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(205, 130,1 58)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 55, 40],
+        },
+        {
+          label: "My Second dataset",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(184, 185, 210, .3)",
+          borderColor: "rgb(35, 26, 136)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(35, 26, 136)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [28, 48, 40, 19, 86, 27, 90],
+        },
+      ],
+    },
+  };
+
+  render() {
+    return (
+      <MDBContainer>
+        <h3 className="mt-5">Line chart</h3>
+        <Line data={this.state.dataLine} options={{ responsive: true }} />
+      </MDBContainer>
+    );
+  }
+}
+
+export default ChartsPage;
+*/

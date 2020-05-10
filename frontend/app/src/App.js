a5c38b02b720c7dd4a7143a992b546d4a2902637
Stella <stella.lee.lzr@gmail.com>
2020-05-09 21:53:22 -0500

    simple home page

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,40 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button';
+// import { connect } from 'react-redux';
+import { Nav, Navbar, Container, Row, Col, Card } from 'react-bootstrap';
+import header_img from "./header.jpeg"
 
 import './App.css';
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
+function NavDropdownExample() {
+  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
 
   return (
-    <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
-    </>
+    <Container fluid className="header-image-style">
+      {/* <Image className="header-style" src={header_img} fluid /> */}
+      <Card className="text-center transparent-0 header-text">
+        {/* <Card.Header>Featured</Card.Header> */}
+        <Card.Body>
+          <Card.Title as="h1" >Covid-19</Card.Title>
+          <Card.Text as="h3">
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Container fluid>
+        <Navbar className="transparent-40" variant="light" style={{ width: 800 }}>
+          {/* <Navbar.Brand href="#home">COVID-19</Navbar.Brand> */}
+          <Nav justify variant="tabs" as="h5" defaultActiveKey="/home">
+            <Nav.Link href="/home">Tab1</Nav.Link>
+            <Nav.Link href="#features">Tab2</Nav.Link>
+            <Nav.Link href="#pricing">Tab3</Nav.Link>
+          </Nav>
+        </Navbar>
+      </Container>
+    </Container>
   );
-};
+}
 
 const App = () => (
-  <Container className="p-3">
-    <Jumbotron>
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast>
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
-    </Jumbotron>
-  </Container>
+    <NavDropdownExample />
 );
 
 export default App;
